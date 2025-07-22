9d29e6658a518e2782069eee4a3f827c1563ed8d
TeAraErueti <keitepai1@gmail.com>
2025-07-23 01:47:07 +1200

    fix error

--- project2/routes/swagger.js
+++ project2/routes/swagger.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require('../swagger.json'); // Adjust path if needed
 
 const router = express.Router();
 
-router.use(swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
+
