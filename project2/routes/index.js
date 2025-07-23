e321ad95af003b76f357e52a169fa4071dd13690
TeAraErueti <keitepai1@gmail.com>
2025-07-23 17:31:08 +1200

    fix typo

--- project2/routes/index.js
+++ project2/routes/index.js
@@ -3,13 +3,11 @@ const router = express.Router();
 
 // Root route for '/'
 router.get('/', (req, res) => {
-  res.json({ 
-    message: "✨ Welcome to the CSE341 Node Project2 API! 🚀",
-    info: "Use the /users endpoint to manage users."
-  });
+  res.type('text/plain').send('✨ Welcome to the CSE341 Node Project2 API! 🚀');
 });
 
 // Your actual API endpoints
 router.use('/users', require('./users'));
 
 module.exports = router;
+
